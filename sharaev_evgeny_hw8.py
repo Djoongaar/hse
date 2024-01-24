1c2af182585065f463aa0477417b37310e506a6f
Evgeny Sharaev <ipsharaev@gmail.com>
2024-01-24 13:34:32 +0700

    Feature is done

--- sharaev_evgeny_hw8.py
+++ sharaev_evgeny_hw8.py
@@ -1,1 +1,15 @@
 # Initial commit
+def sum_distance(begin: int, end: int) -> int:
+    """
+    Function calculates sum of all integers inside given range
+    :param begin: beginning of range
+    :param end: end of the range
+    :return: result
+    """
+    assert isinstance(begin, int), "<{}> should be integer".format(begin)
+    assert isinstance(end, int), "<{}> should be integer".format(begin)
+
+    if begin > end:
+        begin, end = end, begin
+
+    return sum(range(begin, end + 1, 1))
