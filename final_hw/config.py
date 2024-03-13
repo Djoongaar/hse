5a1afbaee00ee6ca0e59d487ac97f87d4a83a8b7
Evgeny Sharaev <ipsharaev@gmail.com>
2024-03-13 19:32:13 +0800

    Final HW

---
+++ final_hw/config.py
@@ -0,0 +1,5 @@
+API_KEY = "8855876211e9e685efb3f3362691ac517d358f47406b2bcf988ab88ef3c40ca5"
+# API_KEY_2 = "8b9e3c37bfc7264ab25fcb8fad3846636cbfd2dc99957d27b21f8ed6ab4b9fd9"
+
+API_FILE_URL = "https://www.virustotal.com/api/v3/files"
+API_FILE_ANALYSES_URL = "https://www.virustotal.com/api/v3/analyses"
