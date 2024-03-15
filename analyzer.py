8690cb430d0b386072a4a79d382051414cf98609
Evgeny Sharaev <ipsharaev@gmail.com>
2024-03-15 17:02:29 +0800

    Create report.json

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import json
+import os
 # (c) [2024] Evgeny S.
 # All rights reserved.
 # Any part of this script can be reproduced, distributed freely
@@ -7,41 +8,46 @@
 # without the prior written permission of the copyright owner.
 
 
-# Manual:
-# Case #1: ./analyzer "invoice-42369643.xlsm"                  # To obtain report in stdout
-# Case #2: ./analyzer "invoice-42369643.xlsm" "report.csv"     # To obtain report in .csv file
-
-
-import sys
+import zipfile
 import requests
 import pandas as pd
 
 
+class Unzipper:
+    def __init__(self):
+        self.__password = 'netology'
+        self.__zip_path = "protected_archive.zip"
+        self.__unzip()
+
+    def __unzip(self):
+        """ Unzip file """
+        if self.__zip_path.endswith(".zip"):
+            directory_to_extract_to = os.path.dirname(os.path.abspath(self.__zip_path))
+            with zipfile.ZipFile(self.__zip_path, 'r') as zip_ref:
+                zip_ref.extractall(directory_to_extract_to, pwd=bytes(self.__password, 'utf-8'))
+
+
 class VirusTotalAnalyzer:
-    def __init__(self, file_path):
-        self.__file_path = file_path
+    def __init__(self):
         self.__api_key = "8855876211e9e685efb3f3362691ac517d358f47406b2bcf988ab88ef3c40ca5"
+        self.__file_path = "invoice-42369643.xlsm"
         self.__upload_file_url = "https://www.virustotal.com/api/v3/files"
         self.__headers = {"accept": "application/json", "x-apikey": self.__api_key}
-        self.__file_id = self.__upload(self.__file_path)
+        self.__file_id = self.__upload()
         self.__analysis_url = "https://www.virustotal.com/api/v3/analyses/{}".format(self.__file_id)
         self.__report_data = self.__analyze()
         self.__report_df = self.__create_df()
 
-    def __unzip(self, path):
-        """ Unzip file """
-        pass
-
-    def __upload(self, path):
+    def __upload(self):
         """
         Upload file into VirusTotal server
-        :param path:
         :return:
         """
-        with open(path, "rb") as file:
-            files = {"file": (path, file)}
+        with open(self.__file_path, "rb") as file:
+            files = {"file": (self.__file_path, file)}
             response = requests.post(self.__upload_file_url, headers=self.__headers, files=files)
-            return response.json()["data"]["id"]
+
+        return response.json()["data"]["id"]
 
     def __analyze(self):
         """
@@ -59,6 +65,9 @@ class VirusTotalAnalyzer:
         """
         data_arr = []
         report = self.__report_data["data"]["attributes"]["results"]
+        if report:
+            with open("report.json", "w") as file:
+                json.dump(report, file, indent=4)
 
         for soft, res in report.items():
             data_arr.append([
@@ -72,24 +81,12 @@ class VirusTotalAnalyzer:
     def to_stdout(self):
         print(self.__report_df)
 
-    def to_csv(self, report_path):
-        return self.__report_df.to_csv(report_path, index=False, sep=";", encoding="utf-8")
-
-    def to_html(self, report_path):
-        """ Method in progress """
-        pass
+    def to_csv(self):
+        return self.__report_df.to_csv("report.csv", index=False, sep=";", encoding="utf-8")
 
 
 if __name__ == "__main__":
-    report_data = VirusTotalAnalyzer(sys.argv[1])
-    try:
-        if sys.argv[2].endswith(".csv"):
-            report_data.to_csv(sys.argv[2])
-        elif sys.argv[2].endswith(".html"):
-            report_data.to_html(sys.argv[2])
-        else:
-            # Send DF to stdout, if format file is not correct
-            report_data.to_stdout()
-    except IndexError:
-        # Send DF to stdout, if filename is not given
-        report_data.to_stdout()
+    Unzipper()
+    df = VirusTotalAnalyzer()
+    df.to_stdout()
+    df.to_csv()
